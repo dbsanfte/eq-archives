0efc5f2f734cfe0ea8dc5590051ef134e1a9cfce
David Sanftenberg <david.sanftenberg@gmail.com>
2021-04-19 21:35:38 +0100

    remove temporary files

--- get-vnboard-topics.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cat $1 | while read id; do
-        wayback_machine_downloader -s -c 4 "http://vnboards.ign.com/message.asp?topic=$id"'*' || echo "error, skipping this one."
-done
-
