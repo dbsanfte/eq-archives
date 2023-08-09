2f63b26dfd5ba86e12316860c1f6be76c90f5ac9
David Sanftenberg <david.sanftenberg@gmail.com>
2023-08-09 11:05:45 +0100

    champions of cabilis

--- capture-links.sh
+++ capture-links.sh
@@ -7,4 +7,14 @@ then
   sourceFile="./links-sanitized-unique.txt"
 fi
 
-cat "$sourceFile" | while read line; do wayback_machine_downloader -s -c 5 -f 19990101000000 -t 20070101000000 $line; done;
+cat "$sourceFile" | while read line; do 
+    if [[ "$line" =~ \.ezboard\.com/ ]]
+    then
+	# Assume an ezboard forum link
+        server=$(echo "$line" | awk -F[/:] '{print $4}')
+        forumName=$(echo "$line" | awk -F[/:] '{print $5}' | cut -c 2-)
+	wayback_machine_downloader -s -c 5 -o "$forumName" -f 19990101000000 -t 20070101000000 "http://${server}"
+    else
+	wayback_machine_downloader -s -c 5 -f 19990101000000 -t 20070101000000 $line 
+    fi
+done
--- sanitize-links.sh
+++ sanitize-links.sh
@@ -13,5 +13,5 @@ then
   destFile="./links-sanitized.txt"
 fi
 
-cat "$sourceFile" | grep -i "href=" | tr '[:upper:]' '[:lower:]' | grep -v "ezboard.com" | sed "s|http://web.archive.org/web/[0-9]*/||g" | grep -oP 'href="(http://.*?)"' | sed 's/href="//g' | sed 's/"//g' >> "$destFile"
+cat "$sourceFile" | grep -i "href=" | tr '[:upper:]' '[:lower:]' | sed "s|http://web.archive.org/web/[0-9]*/||g" | grep -oP 'href="(http://.*?)"' | sed 's/href="//g' | sed 's/"//g' >> "$destFile"
 sort "$destFile" | uniq -u > "$destFile-unique.txt"
